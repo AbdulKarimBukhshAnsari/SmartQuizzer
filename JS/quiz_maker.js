50a9fcb84e209d792fc5ecdb847d232dfed779b6
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-12-22 03:22:41 +0500

    Completed the timer and checker for the correct question

--- JS/quiz_maker.js
+++ JS/quiz_maker.js
@@ -1,7 +1,33 @@
 // this f(unction will make the questions
+let i = 0;
 quiz_make();
+// to next the question
+
+
+// to update the timer 
 function timer() {
   const total_time = 30;
+  let circle = document.querySelector(".circle");
+  circle.innerHTML = `<div class="time text-2xl font-bold absolute mx-auto mt-6 text-customGreen  "></div>
+          <svg class="w-full max-w-[80px] h-auto" viewBox="0 0 100 100">
+            <circle
+              cx="50"
+              cy="50"
+              r="40"
+              fill="transparent"
+              stroke="#f0f0f0"
+              stroke-width="12"
+            ></circle>
+            <circle
+              cx="50"
+              cy="50"
+              r="40"
+              fill="transparent"
+              stroke="rgb(13,148,136)"
+              stroke-width="5"
+              transform="rotate(-90 50 50)"
+            ></circle>
+          </svg>`;
 
   let timer = document.querySelector(".time");
   let animate_circle = document.querySelector("svg circle:nth-child(2)");
@@ -24,31 +50,68 @@ function timer() {
   }, 1000);
 }
 
-function quiz_make() {
-    console.log(quiz)
-  timer();
+
+// for checking whether the answer is correct or not if the answer would be correct then it would make him green or red according to the choden option 
+function checker(answer){
+  let obj = ['first','second' , 'third' , 'fourth'];
+  
+  if(answer == quiz[i].ans){
+    let selected_option = document.querySelector(`#${obj[answer]}`);
+    selected_option.style.backgroundColor = '#E5F8E5';
+  }
+  else{
+    let selected_option = document.querySelector(`#${obj[answer]}`);
+    let correct_answer = document.querySelector(`#${obj[quiz[i].ans]}`);
+    selected_option.style.backgroundColor = '#FFEBEB';
+    correct_answer.style.backgroundColor = '#E5F8E5';
+  }
+
   let options_html = document.querySelector(".main");
-  options_html.innerHTML += `
-        <div class="question text-xl md:text-2xl mt-4 font-semibold text-purple-800 ">${quiz[1].question}</div>
+  options_html.innerHTML += `<div class='flex justify-end'>
+                              <button
+                                      class="nextbutton text-white bg-customGreen lg:py-4 lg:px-14 py-3 px-10 hover:bg-teal-700 rounded-lg font-bold lg:text-2xl text-xl mt-5"
+                                    >
+                                    Next
+                              </button>
+                            </div>`;
+  // add event lsitener here 
+  let next_button = document.querySelector(".nextbutton");
+  next_button.addEventListener("click", function () {
+    alert("Button click hua saale");
+    i += 1;
+    quiz_make();
+  });
+}
+
+// this function will make the quiz 
+function quiz_make() {
+  timer();
+  //to get the element where we have to insert the qustions and the options and the button 
+  let options_html = document.querySelector(".main");
+  options_html.innerHTML = `
+        <div class="question text-xl md:text-2xl mt-4 font-semibold text-purple-800">${quiz[i].question}</div>
         <!-- For the options -->
         <div class="options flex flex-col gap-4 mt-4">
-          
-        </div><div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition">
-            <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">A</div>
-            <div class="ans text-gray-700 font-semibold">${quiz[1].options[0]}</div>
-          </div>
-          
-          <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition">
-            <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">A</div>
-            <div class="ans text-gray-700 font-semibold">${quiz[1].options[1]}</div>
-          </div>
-          <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition">
-            <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">A</div>
-            <div class="ans text-gray-700 font-semibold">${quiz[1].options[2]}</div>
-          </div>
-          <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition">
-            <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">A</div>
-            <div class="ans text-gray-700 font-semibold">${quiz[1].options[3]}</div>
-          </div>`;
- 
+        <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition" id='first' onclick='checker(0)'>
+          <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">A</div>
+          <div class="ans text-gray-700 font-semibold">${quiz[i].options[0]}</div>
+        </div>
+
+        <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition" id='second' onclick='checker(1)'>
+          <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">B</div>
+          <div class="ans text-gray-700 font-semibold">${quiz[i].options[1]}</div>
+        </div>
+
+        <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition" id='third' onclick='checker(2)'>
+          <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">C</div>
+          <div class="ans text-gray-700 font-semibold">${quiz[i].options[2]}</div>
+        </div>
+
+        <div class="option flex items-center border border-gray-300 rounded-lg py-4 px-4 gap-4 cursor-pointer hover:border-teal-400 hover:bg-gray-50 transition" id='fourth' onclick='checker(3)'>
+          <div class="option_name rounded-full bg-gray-200 text-black font-medium px-3 py-1">D</div>
+          <div class="ans text-gray-700 font-semibold">${quiz[i].options[3]}</div>
+        </div>
+      </div>
+`;
+  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html", "JS/**/*.js"],
   theme: {
     extend: {
       colors: {
